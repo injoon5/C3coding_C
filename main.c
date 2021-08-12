83100a7cafba89e5ab5be592c26750a19c6f4e0f
injoon <injoon5@naver.com>
2021-08-12 13:09:00 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,12 +5,6 @@
 
 int main()
 {
-  char *sp = "Hello String";
-  char *name = "씨큐브코딩";
-
-  printf("%d %d\n", sp, "Hello String");
-  printf("%d %d\n", name, "씨큐브코딩");
-  printf("%s %s\n", sp, "Hello String");
-  printf("%s %s\n", name, "씨큐브코딩");
+  printf("hi!");
   return 0;
 }
